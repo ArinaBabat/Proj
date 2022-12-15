f47cf510d39a343bbcbd20487c490b1851620c50
ArinaBabat <st092157@student.spbu.ru>
2022-12-15 21:05:05 +0300

    Добавлены базы данных

---
+++ code/server/models/models.js
@@ -0,0 +1,75 @@
+const sequelize = require('../db')
+const {DataTypes} = require('sequelize')
+const Pacients = sequelize.define('pacient', {
+    pacient_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    first_name: {type: DataTypes.STRING},
+    last_name: {type: DataTypes.STRING},
+    addres_id:{type: DataTypes.INTEGER},
+    phone: {type: DataTypes.INTEGER, unique: true, allowNull: false},
+    password: {type: DataTypes.STRING},
+})
+const Doctors = sequelize.define('doctor', {
+    doctor_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    first_name: {type: DataTypes.STRING, allowNull: false},
+    last_name: {type: DataTypes.STRING, allowNull: false},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: "DOCTOR"},
+})
+const Timetable = sequelize.define('timetable', {
+    timetable_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    day: {type: DataTypes.STRING, allowNull: false},
+    start_of_admission: {type: DataTypes.INTEGER, allowNull: false},
+    end_of_reception:{type: DataTypes.INTEGER, allowNull: false},
+})
+const Records = sequelize.define('record', {
+    record_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    time:{type: DataTypes.INTEGER},
+})
+const Prescriptions = sequelize.define('prescription', {
+    diagnostic: {type: DataTypes.STRING},
+    therapy: {type: DataTypes.STRING},
+})
+const Cabinets = sequelize.define('cabinet', {
+    cabinet_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    number:{type: DataTypes.INTEGER, allowNull: false},
+})
+const Specialties = sequelize.define('speciality', {
+    speciality_id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+Doctors.hasOne(Pacients)
+Pacients.belongsTo(Doctors)
+
+Pacients.hasMany(Records)
+Records.belongsTo(Pacients)
+
+Doctors.hasMany(Records)
+Records.belongsTo(Doctors)
+
+Timetable.hasMany(Records)
+Records.belongsTo(Timetable)
+
+Records.hasOne(Prescriptions)
+Prescriptions.belongsTo(Records)
+
+Cabinets.hasMany(Timetable)
+Timetable.belongsTo(Cabinets)
+
+Doctors.hasMany(Timetable)
+Timetable.belongsTo(Doctors)
+
+Specialties.hasMany(Cabinets)
+Cabinets.belongsTo(Specialties)
+
+Specialties.hasMany(Doctors)
+Doctors.belongsTo(Specialties)
+
+module.exports = {
+    Pacients,
+    Doctors,
+    Timetable,
+    Records,
+    Prescriptions,
+    Cabinets,
+    Specialties
+}
