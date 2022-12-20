b584cacb152315a3b37b88c02686bd9c5c8b1b9d
ArinaBabat <st092157@student.spbu.ru>
2022-12-21 01:02:02 +0300

    q

---
+++ code/client/src/hospital/userhospital.js
@@ -0,0 +1,29 @@
+import {makeAutoObservable} from "mobx";
+export default class UserHospital{
+  constructor() {
+    this._isAuth = false
+    this._isDoc = false
+    this._user = {}
+    makeAutoObservable(this)
+  }
+
+  setIsAuth(bool) {
+    this._isAuth = bool
+  }
+  setIsDoc(bool) {
+    this._isDoc = bool
+  }
+  setUser(user) {
+    this._user = user
+  }
+
+  get isAuth() {
+    return this._isAuth
+  }
+  get isDoc() {
+    return this._isDoc
+  }
+  get user() {
+    return this._user
+  }
+}
--- code/client/src/index.js
+++ code/client/src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
 import App from './App';
-//import reportWebVitals from './reportWebVitals';
-
+import UserHospital from "./hospital/userhospital";
+export const Context = createContext(null)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-//  <React.StrictMode>
-    <App />
-//  </React.StrictMode>
+  <Context.Provider value={{
+    user: new UserHospital(),
+    }}>
+      <App />
+    </Context.Provider>,
+  document.getElementById('root')
 );
-
-//reportWebVitals();
