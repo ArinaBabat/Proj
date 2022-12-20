6ccb1b272b0cf44f316de0f92b06994e80ac08fa
ArinaBabat <st092157@student.spbu.ru>
2022-12-20 23:33:11 +0300

    fr

--- code/client/src/components/AppRouter.js
+++ code/client/src/components/AppRouter.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {Routes, Roure, Navigate} from 'react-router-dom'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import {authRoutes, publicRoutes} from "../routes";
 const AppRouter = () => {
+  const isAuth = false
   return (
-    <div>
-    
-    </div>
+    <Routes>
+      {isAuth && authRoutes.map(({path, Component}) =>
+        <Route key={path} path={path} element={<Component/>} exact/>
+      )}
+      {publicRoutes.map(({path, Component}) =>
+      <Route key={path} path={path} element={<Component/>} exact/>
+      )}
+    </Routes>
   );
 };
 export default AppRouter;
--- code/client/src/routes.js
+++ code/client/src/routes.js
@@ -1,4 +1,12 @@
-import {HEAD_PHYSICIAN_ROUTE,DOCTOR_LOGIN_ROUTE,PACIENT_LOGIN_ROUTE,PACIENT_REGISTRATION_ROUTE,DOCTOR_TIMETABLE_ROUTE NEW_RECORD_ROUTE, DOCTOR_PACIENTS_ROUTE, PRESCRIPTIONS_ROUTE, PACIENT_RECORDS_ROUTE, TIMETABLE_ROUTE} from "./utils/consts";
+import {HEAD_PHYSICIAN_ROUTE, DOCTOR_LOGIN_ROUTE, PACIENT_LOGIN_ROUTE, PACIENT_REGISTRATION_ROUTE,DOCTOR_TIMETABLE_ROUTE, NEW_RECORD_ROUTE, DOCTOR_PACIENTS_ROUTE, PRESCRIPTIONS_ROUTE, PACIENT_RECORDS_ROUTE, TIMETABLE_ROUTE} from "./utils/consts";
+import Auth from "./pages/Auth";
+import Timetable from "./pages/Timetable";
+import HeadPhysician from "./pages/HeadPhysician";
+import Pacients from "./pages/Pacients";
+import MyTimetable from "./pages/MyTimetable";
+import Records from "./pages/Records";
+import NewRecord from "./pages/NewRecord";
+import Prescriptions from "./pages/Prescriptions";
 export const authRoutes = [
   {
     path: TIMETABLE_ROUTE,
