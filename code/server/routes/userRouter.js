de32611cc9303302789340a50eb04387967b9a0f
ArinaBabat <st092157@student.spbu.ru>
2023-03-13 19:11:50 +0300

    q

--- code/server/routes/userRouter.js
+++
@@ -1,6 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const authMiddleware = require('../middleware/authMiddleware')
-const userController = require('../controllers/userController.js')
-router.get('/auth',authMiddleware,userController.check)
-module.exports = router
